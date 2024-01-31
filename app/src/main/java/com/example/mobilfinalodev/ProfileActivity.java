e26d11c57d1918365e09f02018a2f5c5cfcd6fa0
Mehmet Muharrem Ceylan <mehmetmuharrem1919@gmail.com>
2024-01-31 23:39:55 +0300

    Mobil Ödev
    Drawer menu ve profil sayfası eklendi

--- app/src/main/java/com/example/mobilfinalodev/ProfileActivity.java
+++ app/src/main/java/com/example/mobilfinalodev/ProfileActivity.java
@@ -14,7 +14,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.material.badge.BadgeUtils;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -25,57 +24,48 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfileActivity extends AppCompatActivity {
 
-    Button btn_Logout,btn_Guncelle;
-    EditText editName, editSurname, editAcıklama;
+    DrawerLayout drawer;
+    Button btn_Guncelle;
+    EditText edit_isim, edit_Soyisim, edit_Acıklama, edit_Email;
     DatabaseReference mReference;
     FirebaseUser mUser;
     FirebaseAuth mAuth;
     HashMap<String, Object> mData;
-
-    DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        /*btn_Logout= findViewById(R.id.btn_Logout);
+        setContentView(R.layout.activity_profile);
+
+        drawer= findViewById(R.id.drawer_background);
+
         btn_Guncelle= findViewById(R.id.btn_Guncelle);
-        editName= findViewById(R.id.editName);
-        editSurname= findViewById(R.id.editSurname);
-        editAcıklama= findViewById(R.id.editAcıklama);*/
+        edit_isim= findViewById(R.id.edit_isim);
+        edit_Soyisim= findViewById(R.id.edit_Soyisim);
+        edit_Acıklama= findViewById(R.id.edit_Acıklama);
+        edit_Email= findViewById(R.id.edit_Email);
 
         mAuth=FirebaseAuth.getInstance();
         mUser = mAuth.getCurrentUser();
 
-        drawer= findViewById(R.id.drawer_background);
-
-        /*if(mUser==null){
-            Intent intent= new Intent(MainActivity.this, Login.class);
+        if(mUser==null){
+            Intent intent= new Intent(ProfileActivity.this, Login.class);
             startActivity(intent);
             finish();
         }else{
+            edit_Email.setText(mUser.getEmail());
             verileriGetir(mUser.getUid());
-        }*/
-
-        /*btn_Logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-                Intent intent= new Intent(MainActivity.this, Login.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        }
 
         btn_Guncelle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mData = new HashMap<>();
                 String isim, soyisim, acıklama;
-                isim= String.valueOf(editName.getText());
-                soyisim= String.valueOf(editSurname.getText());
-                acıklama= String.valueOf(editAcıklama.getText());
+                isim= String.valueOf(edit_isim.getText());
+                soyisim= String.valueOf(edit_Soyisim.getText());
+                acıklama= String.valueOf(edit_Acıklama.getText());
 
                 mData.put("isim",isim);
                 mData.put("soyisim",soyisim);
@@ -83,8 +73,82 @@ public class MainActivity extends AppCompatActivity {
 
                 verileriGuncelle(mData, mUser.getUid());
             }
-        });*/
+        });
     }
+
+    public void verileriGuncelle(HashMap<String, Object> data, String uid){
+        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(uid);
+        mReference.updateChildren(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(ProfileActivity.this,"Güncelleme Başarılı",Toast.LENGTH_SHORT);
+                    verileriGetir(uid);
+                }
+            }
+        });
+
+    }
+    public void verileriGetir(String uid){
+        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(uid).child("isim");
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value= snapshot.getValue(String.class);
+                edit_isim.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(mUser.getUid()).child("soyisim");
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value= snapshot.getValue(String.class);
+                edit_Soyisim.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(mUser.getUid()).child("acıklama");
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value= snapshot.getValue(String.class);
+                edit_Acıklama.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(uid).child("email");
+        mReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value= snapshot.getValue(String.class);
+                edit_Email.setText(value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+
+
+
+
+
 
     public void menuClick(View view){
         drawerOpen(drawer);
@@ -105,80 +169,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void anasayfaClick(View view){
-        drawerClose(drawer);
-        recreate();
-
+        Intent intent= new Intent(ProfileActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 
     public void profileClick(View view){
-        Intent intent= new Intent(MainActivity.this, ProfileActivity.class);
-        startActivity(intent);
-        finish();
+        drawerClose(drawer);
+        recreate();
     }
 
     public void logoutClick(View view){
         FirebaseAuth.getInstance().signOut();
-        Intent intent= new Intent(MainActivity.this, Login.class);
+        Intent intent= new Intent(ProfileActivity.this, Login.class);
         startActivity(intent);
         finish();
     }
-
-
-    public void verileriGuncelle(HashMap<String, Object> data, String uid){
-        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(uid);
-        mReference.updateChildren(data).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                if(task.isSuccessful()){
-                    Toast.makeText(MainActivity.this,"Güncelleme Başarılı",Toast.LENGTH_SHORT);
-                    verileriGetir(uid);
-                }
-            }
-        });
-
-    }
-
-
-    public void verileriGetir(String uid){
-        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(uid).child("isim");
-        mReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String value= snapshot.getValue(String.class);
-                editName.setText(value);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(mUser.getUid()).child("soyisim");
-        mReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String value= snapshot.getValue(String.class);
-                editSurname.setText(value);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-        mReference= FirebaseDatabase.getInstance().getReference("kullanıcı").child(mUser.getUid()).child("acıklama");
-        mReference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String value= snapshot.getValue(String.class);
-                editAcıklama.setText(value);
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        });
-    }
-
 }
